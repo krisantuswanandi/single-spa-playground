e8f566f4ca535c322f79e3d5cc06896b1d7d8d46
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2024-07-05 16:47:01 +0700

    integrate with container spa

--- container-spa/src/playground-root-config.ts
+++ container-spa/src/playground-root-config.ts
@@ -1,19 +1,19 @@
 import { registerApplication, start, LifeCycles } from "single-spa";
 
-registerApplication({
-  name: "@single-spa/welcome",
-  app: () =>
-    System.import<LifeCycles>(
-      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-    ),
-  activeWhen: ["/"],
-});
-
 // registerApplication({
-//   name: "@playground/navbar",
-//   app: () => System.import("@playground/navbar"),
-//   activeWhen: ["/"]
+//   name: "@single-spa/welcome",
+//   app: () =>
+//     System.import<LifeCycles>(
+//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+//     ),
+//   activeWhen: ["/"],
 // });
+
+registerApplication({
+  name: "@playground/component",
+  app: () => System.import<LifeCycles>("//localhost:8080/js/app.js"),
+  activeWhen: ["/"]
+});
 
 start({
   urlRerouteOnly: true,
